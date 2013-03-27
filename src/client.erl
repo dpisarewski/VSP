dba53192d2c69207a7d1c42843f4828756a8dfb6
denisfleischhauer <d.fleischhauer@gmx.de>
2013-03-27 11:28:28 +0100

    SDF

---
+++ src/client.erl
@@ -0,0 +1,35 @@
+%% Copyright
+-module(client).
+-author("denisfleischhauer").
+
+%% API
+-export([start/1]).
+
+start(ServerPID) ->
+  {ok, ConfigListe} = file:consult("cfg/client.cfg"),
+  {ok, LogDatei} = werkzeug:get_config_value(log_datei, ConfigListe),
+
+  spawn(fun() -> simulation(5, ServerPID, LogDatei) end)
+.
+
+simulation(AnzahlSchritte, ServerPID, LogDatei) ->
+  ServerPID ! {getmsgid, self()},
+  werkzeug:logging(LogDatei,"-Client: Nachrichtennummer von " ++pid_to_list(ServerPID)++ " angefragt\n"),
+  receive
+    {nnr, Number} ->
+      werkzeug:logging(LogDatei,"-Client: Nachrichtennummer: " ++integer_to_list(Number)++ " von " ++pid_to_list(ServerPID)++ " bekommen\n"),
+
+      {ok, Hostname} = inet:gethostname(),
+      Praktikumsgruppe = "1",
+      Teamnummer = "06",
+      Systemsendezeit = werkzeug:timeMilliSecond(),
+      Nachricht = "0-client@"
+        ++Hostname++Praktikumsgruppe++Teamnummer++" : "++Number++"te Nachricht "++Systemsendezeit++"\n",
+
+      ServerPID ! {dropmessage, {Nachricht, Number}},
+      werkzeug:logging(LogDatei,"-Client: Nachricht: " ++integer_to_list(Number)++ " an " ++pid_to_list(ServerPID)++ " gesendet\n")
+  end
+
+  %{reply,Number,Nachricht,Terminated} ->
+.
+
