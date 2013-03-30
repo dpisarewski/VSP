504ba331d3dc15e20d6702c66121cd16b1db4c2b
Dieter Pisarewski <dieter.pisarewski@gmail.com>
2013-03-30 07:58:30 +0100

    refactor client manager

--- src/client_manager.erl
+++ src/client_manager.erl
@@ -3,38 +3,11 @@
 
 loop(ClientList) ->
   receive
-    {subscribe_client, ClientPID} ->                                    %% Traegt neue Client in die ClientListe. Nr = 0, weil Client neu ist.
-                                                                  %% ClientList erhaelt die Tupeln {ClientPID, Nr}, wo "Nr" die Nummer der letzte empfangene Nachricht ist.
-      tools:stdout("Client " ++ werkzeug:to_String(ClientPID) ++ " ist in die Liste eingefuegt.~n"),
-      loop(lists:append(ClientList, [{ClientPID, 0}]));
+    {set_client_info, {ClientPid, Number, Timestamp}} ->
+      loop(lists:keyreplace(ClientPid, 1, ClientList, {ClientPid, Number, Timestamp}));
 
-    {unsubscribe_client, ClientPID} ->
-      tools:stdout("Client " ++ werkzeug:to_String(ClientPID) ++ " ist aus der Liste entfernt.~n"),
-      loop(delete_client_with_pid(ClientList, ClientPID));
-
-    {increment_message_number, ClientPID} ->
-      {_, CurrentNumber} = lists:keyfind(ClientPID, 1, ClientList),
-      tools:stdout("Nachrichtennummer fuer Client " ++ werkzeug:to_String(ClientPID) ++ " inkrementiert~n"),
-      loop(lists:keyreplace(ClientPID, 1, ClientList, {ClientPID, CurrentNumber + 1}));
-
-    {next_message_number, ServerPID, ClientPID} ->                           %% Gibt die Nummer der letzte an den Client gesendete Nachricht.
-      Client = find_client(ClientList, ClientPID),
-      {_, Number} = Client,
-      ServerPID ! {next_message_number, ClientPID, Number},
-      tools:stdout("Client " ++ werkzeug:to_String(ClientPID) ++ " hat die letzte Nachricht mit der Nummer " ++ werkzeug:to_String(Number) ++ " empfangen.~n"),
-      loop(ClientList);
-
-    {get_client_list, Pid} ->
-      Pid ! {client_list, ClientList},
+    {get_client_info, Pid, ClientPid} ->
+      Pid ! {client_info, lists:keyfind(ClientPid, 1, ClientList)},
       loop(ClientList)
   end
-.
-
-find_client(ClientList, ClientPID) ->
-  lists:keyfind(ClientPID, 1, ClientList)
-.
-
-
-delete_client_with_pid(ClientList, ClientPID) ->
-  lists:keydelete(ClientPID, 1, ClientList)
 .
