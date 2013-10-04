f921f6275a9e57a22ab874420d12c658a790bda1
Dieter Pisarewski <dieter@pisarewski.info>
2013-10-04 13:45:21 +0200

    loop for client

--- src/client.erl
+++ src/client.erl
@@ -3,19 +3,23 @@
 -author("Dieter Pisarewski, Maxim Rjabenko").
 -compile([debug_info, export_all]).
 
-start(ClientNumber) ->
-  spawn(loop)
+start(ServerNode, ClientNumber) ->
+  ServerName  = tools:get_config_value(client, servername),
+  Server      = {ServerName, ServerNode},
+  spawn(fun() -> loop(Server, ClientNumber, 1) end)
 .
 
-loop(ClientNumber, MessageNumber) ->
+loop(Server, ClientNumber, MessageNumber) ->
   Message = generate_message(ClientNumber, MessageNumber),
-  send_message(Message)
+  send_message(Server, Message),
+  loop(Server, ClientNumber, MessageNumber + 1)
 .
 
-send_message(Message) ->
+send_message(Server, Message) ->
   stub
 .
 
 generate_message(ClientNumber, MessageNumber) ->
-  stub
+  {ok, Hostname} = inet:gethostname(),
+  lists:concat([ClientNumber, "-client@", Hostname, " 1", "-08", " : ", MessageNumber, "te Nachricht. C Out: ", werkzeug:timeMilliSecond()])
 .
